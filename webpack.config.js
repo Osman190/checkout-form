1f70545325ddd2c2a29c7f3085a4ebacd1242c93
Elbehary <ahmedelbehary0@gmail.com>
2018-03-06 14:10:18 +0100

    add pug, js and css compiled in dist folder me in prograss :)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+var config = {
   entry: './src/js/index.js',
   output: {
-    filename: './dist/bundle.js'
-  }
-};
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '!!pug-loader!src/template/index.pug',
+    }),
+  ]
+}
+module.exports = config;
